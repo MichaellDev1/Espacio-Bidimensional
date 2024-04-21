7db4b514ab57719c13708e5c4c50a115a14da9be
MichaellDev1 <santuchomichael131@gmail.com>
2024-04-21 19:46:35 -0300

    build canvas function - create coordinate axes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,58 @@
+class Canvas {
+    canvas: null | HTMLCanvasElement
+    context = CanvasRenderingContext2D
+
+    constructor() {
+        this.canvas = null
+    }
+
+    buildCanvas(id: string) {
+        const canvas = document.createElement('canvas')
+        canvas.id = id
+        document.body.appendChild(canvas)
+        this.canvas = canvas
+        return canvas
+    }
+}
+
+const createCanvas = (id: string): HTMLCanvasElement => {
+    const canvas = new Canvas()
+    return canvas.buildCanvas(id)
+}
+
+class CoordinateAxes {
+    canvasWidth: number
+    canvasHeight: number
+
+    constructor(canvasWidth: number = 0, canvasHeight: number = 0) {
+        this.canvasHeight = canvasHeight
+        this.canvasWidth = canvasWidth
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        // Build axe Y
+        context.fillStyle = '#1f1f1f'
+        context.fillRect(this.canvasWidth / 2, 0, 1, this.canvasHeight)
+
+        // Build axe X
+        context.fillStyle = '#1f1f1f'
+        context.fillRect(0, this.canvasHeight / 2, this.canvasWidth, 1)
+    }
+}
+
 window.addEventListener('load', function () {
-    const canvas: HTMLCanvasElement = document.getElementById('space') as HTMLCanvasElement
+    const canvas = createCanvas('space')
     const ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D
 
     const canvasWidth: number = 900;
-    const canvasHeight: number = 600;
+    const canvasHeight: number = 700;
 
     canvas.width = canvasWidth
     canvas.height = canvasHeight
 
     ctx.fillStyle = '#393939'
     ctx.fillRect(0, 0, canvasWidth, canvasHeight)
+
+    const coordinateAxes = new CoordinateAxes(canvasWidth, canvasHeight)
+    coordinateAxes.draw(ctx)
 })
